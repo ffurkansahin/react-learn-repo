e6dad15c57bc635128fe6e54e07fe3d8d45dc06a
Furkan Şahin <furkan.sahinbusiness@gmail.com>
2025-01-25 15:04:48 +0300

    first react codes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      First Web Page With React
+      <img src='/logo192.png'></img>
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
